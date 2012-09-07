40cfa8aa918cf99c94721735c062e18c16ed235d
David Berkman <david.berkman@grenadefish.net>
2012-09-06 20:47:16 -0700

    we don't get to be temporary

--- dojo/src/main/java/com/wicketized/dojo/DojoLazyLoadPaneBehavior.java
+++ dojo/src/main/java/com/wicketized/dojo/DojoLazyLoadPaneBehavior.java
@@ -1,12 +1,10 @@
 package com.wicketized.dojo;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
 public class DojoLazyLoadPaneBehavior extends DojoOnEventAjaxBehavior {
 
   private DojoLazyLoadPane dojoLazyLoadPane;
-  private boolean completed = false;
 
   public DojoLazyLoadPaneBehavior (DojoLazyLoadPane dojoLazyLoadPane, String event) {
 
@@ -19,12 +17,5 @@ public class DojoLazyLoadPaneBehavior extends DojoOnEventAjaxBehavior {
   protected void onEvent (AjaxRequestTarget target) {
 
     dojoLazyLoadPane.loadPanel(target);
-    completed = true;
-  }
-
-  @Override
-  public boolean isTemporary (Component component) {
-
-    return completed;
   }
 }
