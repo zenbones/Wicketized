5f4704c2f998ea20fe40d347556bcb18f3924008
David Berkman <david.berkman@grenadefish.net>
2012-09-06 20:04:44 -0700

    ease of use panel loading behavior

---
+++ dojo/src/main/java/com/wicketized/dojo/DojoLazyLoadPaneBehavior.java
@@ -0,0 +1,12 @@
+package com.wicketized.dojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 9/6/12
+ * Time: 7:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DojoLazyLoadPaneBehavior {
+
+}
