af541251fa3c1f3e1747fde1ffabcf639c7607a1
David Berkman <david.berkman@grenadefish.net>
2012-09-06 20:08:28 -0700

    behavior update (made temporary)

--- dojo/src/main/java/com/wicketized/dojo/DojoLazyLoadPaneBehavior.java
+++ dojo/src/main/java/com/wicketized/dojo/DojoLazyLoadPaneBehavior.java
@@ -1,12 +1,30 @@
 package com.wicketized.dojo;
 
-/**
- * Created with IntelliJ IDEA.
- * User: david.berkman
- * Date: 9/6/12
- * Time: 7:59 PM
- * To change this template use File | Settings | File Templates.
- */
-public class DojoLazyLoadPaneBehavior {
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 
+public class DojoLazyLoadPaneBehavior extends DojoOnEventAjaxBehavior {
+
+  private DojoLazyLoadPane dojoLazyLoadPane;
+  private boolean completed = false;
+
+  public DojoLazyLoadPaneBehavior (DojoLazyLoadPane dojoLazyLoadPane, String event) {
+
+    super(event);
+
+    this.dojoLazyLoadPane = dojoLazyLoadPane;
+  }
+
+  @Override
+  protected void onEvent (AjaxRequestTarget target) {
+
+    dojoLazyLoadPane.loadPanel(target);
+    completed = true;
+  }
+
+  @Override
+  public boolean isTemporary (Component component) {
+
+    return completed;
+  }
 }
